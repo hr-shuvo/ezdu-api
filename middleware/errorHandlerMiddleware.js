6b7dee70eb620f16a811bdfa40f224e192db4b13
hr-shuvo <h.r.shuvo9@gmail.com>
2025-02-10 16:21:21 +0600

    initial setup

---
+++ middleware/errorHandlerMiddleware.js
@@ -0,0 +1,23 @@
+import {StatusCodes} from "http-status-codes";
+
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        message: err.message || 'Something went wrong',
+    }
+
+    if (err.name === 'ValidationError') {
+        customError.message = Object.values(err.errors).map((item) => item.message).join(',');
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+    }
+
+    if (err.code && err.code === 11000) {
+        customError.message = `Duplicate value entered for ${Object.keys(err.keyValue)}`;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+    }
+
+    return res.status(customError.statusCode).json({msg: customError.message});
+};
+
+export default errorHandlerMiddleware;
---
+++ server.js
@@ -0,0 +1,53 @@
+import * as dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+
+
+dotenv.config();
+const app = express();
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors({ origin: process.env.CLIENT_URL.split(','), credentials: true }));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello EZDU');
+});
+
+app.get('/api/v1', (req, res) => {
+    res.json({msg:'API is running'});
+});
+
+// Router
+
+
+
+
+app.use('*', (req, res) => {
+    res.status(404).json({error: 'route not found'});
+});
+
+
+// Middlewares
+app.use(errorHandlerMiddleware);
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+try {
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+} catch (e) {
+    console.error(e);
+    process.exit(1);
+}
