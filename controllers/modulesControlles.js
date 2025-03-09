46e719c7f4e0d1402bbce31d2c1fa947925231a3
hr-shuvo <h.r.shuvo9@gmail.com>
2025-03-09 07:48:26 +0600

    add pagination - modules

--- controllers/modulesControlles.js
+++ controllers/modulesControlles.js
@@ -2,10 +2,26 @@ import { Module } from "../models/CourseModel.js";
 import { _loadModules } from "../services/moduleService.js";
 
 export const loadModules = async (req, res) => {
-    try {
-        const data = await _loadModules();
 
-        res.status(200).json(data);
+
+    const {isActive} = req.query;
+
+    const query = {};
+
+    if(isActive == true){
+        query.status = 1;
+    }
+    if(isActive == false){
+        query.status = 0;
+    }
+
+    const page = Number(req.query.pg) || 1;
+    const size = Number(req.query.sz) || 10;
+
+    try {
+        const {data, totalCount, totalPage, currentPage} = await _loadModules(query, page, size);
+
+        res.status(200).json({data, totalCount, totalPage, currentPage});
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch modues", error: error.message });
     }
--- services/moduleService.js
+++ services/moduleService.js
@@ -1,14 +1,20 @@
 import { Module } from '../models/CourseModel.js'
 
-export const _loadModules = async () => {
+export const _loadModules = async (query, page, size) => {
     try {
-        const data = await Module.find();
+
+        const skip = (page-1) *  size;
+
+        const data = await Module.find(query).skip(skip).limit(size);
+
+        const totalCount = await Module.countDocuments(query);
+        const totalPage = Math.ceil(totalCount / size);
 
         if (!data) {
             throw new NotFoundError('modules not found')
         }
 
-        return data;
+        return {data, totalCount, totalPage, currentPage:page};
 
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch modules", error: error.message });
