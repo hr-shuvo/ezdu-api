93b98c850ca004183483ade814a31b1d2ce0e789
hr-shuvo <h.r.shuvo9@gmail.com>
2025-03-16 03:42:15 +0600

    get lesson

--- controllers/lessonController.js
+++ controllers/lessonController.js
@@ -1,3 +1,4 @@
+import { Lesson } from "../models/CourseModel.js";
 import { _loadLessons } from "../services/lessonService.js";
 
 export const loadLessons = async (req, res) => {
@@ -24,5 +25,26 @@ export const loadLessons = async (req, res) => {
         res.status(200).json({data, totalCount, totalPage, currentPage});
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch modues", error: error.message });
+    }
+}
+
+
+
+export const getLesson = async (req, res) => {
+    try {
+        const lessonId = req.params.id;
+        if(!lessonId){
+            return res.status(400).json({ message: "Invalid lesson id", error: error.message });
+        }
+
+        const lesson = await Lesson.findById(lessonId);
+
+        if(!lesson){
+            return res.status(404).json({ message: "Failed to fetch lesson", error: error.message });
+        }
+
+        res.status(200).json(lesson);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to fetch lesson", error: error.message });
     }
 }
--- routes/lessonRoute.js
+++ routes/lessonRoute.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { loadLessons } from "../controllers/lessonController.js";
+import { loadLessons, getLesson } from "../controllers/lessonController.js";
 
 const router = Router();
 
 
 
 router.get('/', loadLessons);
-// router.get('/:id', getLesson);
+router.get('/:id', getLesson);
 // router.post('/create', createLesson); // upsert
 
 
