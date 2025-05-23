cd8aeea74e885782021d93f0814b70364df8e95c
hr-shuvo <h.r.shuvo9@gmail.com>
2025-05-24 04:41:54 +0600

    fix

--- controllers/acadymy/academyClassController.js
+++ controllers/acadymy/academyClassController.js
@@ -4,12 +4,22 @@ import { AcademyClass } from "../../models/AcademyModel.js";
 
 
 export const loadAcademicClass = async (req, res) => {
-
-    const query = req.query;
+    const {} = req.query;
 
     const page = Number(req.query.pg) || 1;
     const size = Number(req.query.sz) || 10;
 
+    const query = {};
+
+    if(req.query.level){
+        query.level = req.query.level;
+    }
+
+    if(req.query.version){
+        query.version = req.query.version;
+    }
+
+    // console.log(query);
 
     try {
         // const data = await Course.find();
@@ -24,19 +34,20 @@ export const loadAcademicClass = async (req, res) => {
 
 
 export const getAcademicClass = async (req, res) => {
+
     try {
-        const classId = req.params.id;
-        if (!classId) {
-            return res.status(400).json({message: "Invalid course id"});
+        const id = req.params.id;
+        if (!id) {
+            return res.status(400).json({message: "Invalid class id"});
         }
 
-        const academicClass = await AcademyClass.findById(classId);
+        const result = await AcademyClass.findById(id);
 
-        if (!academicClass) {
+        if (!result) {
             return res.status(404).json({message: "Failed to fetch class"});
         }
 
-        res.status(200).json(course);
+        res.status(200).json(result);
     } catch (error) {
         res.status(500).json({message: "Failed to fetch class", error: error.message});
     }
